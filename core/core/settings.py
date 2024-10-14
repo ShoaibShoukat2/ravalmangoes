da062fe44e98519fdd935a91f5fbf34bb2171032
Shoaib <shoaibshoukatbhatti99@gmail.com>
2024-10-14 11:50:32 +0500

    add domains

--- core/core/settings.py
+++ core/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-odprln%og7e&lmzsr_v@+#zd*q5gedmc+%k1d8!y12fo60jay8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000','ravalmangoes.pythonanywhere.com']
 
 
 # 'shoaibahmad.pythonanywhere.com'
